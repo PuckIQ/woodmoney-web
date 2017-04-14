f85b2e547e7f6ccef945a079f67461b714483eb4
Derek Blasutti <dawgbone98@gmail.com>
2017-04-14 14:06:00 -0400

    Added positions toggles

--- resources/views/pages/teams.blade.php
+++ resources/views/pages/teams.blade.php
@@ -12,7 +12,7 @@
             <h2>{{$team}}</h2>
             <h4>{{$season}}</h4>
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-4">
                 Competition : 
                 <select name="competition" id="competition">
                   <option value="All">All</option>
@@ -24,7 +24,16 @@
                 <select name="gameType" id="gameType">
                   <option value="All">All</option>
                 </select>
-                <button type="button" class="btn btn-xs btn-success" id="CompSelectSubmit"><strong>Go</strong></button>
+                <button type="button" class="btn btn-xs btn-success" id="CompSelectSubmit"><strong>Go</strong></button>                
+                </div>
+                <div class="col-md-4 selector">      
+                  <input type="hidden" id="compSelect" value="All">                
+                  <input type="hidden" id="confSelect" value="All">            
+                  Fowards <input type="checkbox" name="positions" value="F" checked="checked" style="margin-right:5px;" /> 
+                  Centres <input type="checkbox" name="positions" value="C" checked="checked" style="margin-right:5px;"  /> 
+                  LW <input type="checkbox" name="positions" value="L" checked="checked" style="margin-right:5px;"  /> 
+                  RW <input type="checkbox" name="positions" value="R" checked="checked" style="margin-right:5px;" />
+                  Defencemen <input type="checkbox" name="positions" value="D" checked="checked" /> 
                 </div>
             </div>
               <div class="row">
@@ -116,6 +125,8 @@ $(document).ready(function(){
     widgets           : ['zebra', 'columns'],
   }); 
 
+  var parseData = JSON.parse(teamData.replace(/&quot;/g,'"')); 
+
   $("#competition").on('change',function(){
     var competition = $(this).val();
     if(competition == 'All'){
@@ -128,22 +139,60 @@ $(document).ready(function(){
   $("#CompSelectSubmit").click(function(e){
     var competition = $("#competition").val();
     var gameType = $("#gameType").val();
-    var parseData = JSON.parse(teamData.replace(/&quot;/g,'"'));
-    var htmlTable = "";
-    $.each(parseData,function(key,value){
-        if(value.Conf == gameType && value.Comp == competition){
+       
+    var positions = $("input:checked").map(function(){
+      return $(this).val();
+    });
+    $("#compSelect").val($("#competition").val());
+    $("#confSelect").val($("#gameType").val());   
+    updateTable(parseData,competition,gameType,positions.get()); 
+  });
+
+  $(".selector").children("input:checkbox").click(function(e){
+    var position = $(this).val();
+    if(position == "F"){
+      if(this.checked){
+        $("input:checkbox[value='C']").prop('checked', true);
+        $("input:checkbox[value='R']").prop('checked', true);
+        $("input:checkbox[value='L']").prop('checked', true);
+      }else{
+        $("input:checkbox[value='C']").prop('checked', false);
+        $("input:checkbox[value='R']").prop('checked', false);
+        $("input:checkbox[value='L']").prop('checked', false);
+      }
+    }
+
+    if($("input:checkbox[value='C']").prop('checked') && $("input:checkbox[value='L']").prop('checked') && $("input:checkbox[value='R']").prop('checked')){
+      $("input:checkbox[value='F']").prop('checked', true);
+    }else{
+      $("input:checkbox[value='F']").prop('checked', false);
+    }
+
+    var positions = $("input:checked").map(function(){
+      if($(this).val()!=="F"){
+        return $(this).val();
+      }
+    });
+    console.log(positions.get());
+    updateTable(parseData,$("#compSelect").val(),$("#confSelect").val(),positions.get()); 
+  });
+});
+
+function updateTable(parseData,competition,gameType,positions){
+  var htmlTable = "";
+  $.each(parseData,function(key,value){
+      //console.log(value.Pos);
+        if(value.Conf == gameType && value.Comp == competition && $.inArray(value.Pos,positions)!== -1){
           var conference = value['Conf'];
           if(value['Conf']=="Both"){
             conference = "All";
           }
           htmlTable += '<tr><td style="white-space: nowrap;"><a href="{{url('players')}}/'+value['PlayerId']+'">'+value['Player']+'</a></td><td>'+value['Pos']+'</td><td>'+value['Comp']+'</td><td>'+conference+'</td><td>'+Number(value['TOI']/60).toFixed(2)+'</td><td>'+Number(value['CompTOI%']).toFixed(2)+'</td><td>'+value['CF']+'</td><td>'+value['CA']+'</td><td>'+Number(value['CF%']).toFixed(2)+'</td><td>'+Number(value['CF/60']).toFixed(2)+'</td><td>'+Number(value['CA/60']).toFixed(2)+'</td><td>'+Number(value['CF60RelComp']).toFixed(2)+'</td><td>'+Number(value['CA60RelComp']).toFixed(2)+'</td><td>'+Number(value['CF%RelComp']).toFixed(2)+'</td><td>'+Number(value['CF%RelAll']).toFixed(2)+'</td><td>'+Number(value['DFF']).toFixed(2)+'</td><td>'+Number(value['DFA']).toFixed(2)+'</td><td>'+Number(value['DFF%']).toFixed(2)+'</td><td>'+Number(value['DFF/60']).toFixed(2)+'</td><td>'+Number(value['DFA/60']).toFixed(2)+'</td><td>'+Number(value['DFF60RelComp']).toFixed(2)+'</td><td>'+Number(value['DFA60RelComp']).toFixed(2)+'</td><td>'+Number(value['DFF%RelComp']).toFixed(2)+'</td><td>'+Number(value['DFF%RelAll']).toFixed(2)+'</td></tr>';
-        }value['Player']
+        }
     });
     $("#dataTable").html("<table>"+htmlTable+"</table>");
     var resort = true;
-      $("table").trigger("update", [resort]);
-  });
-
-});
+    $("table").trigger("update", [resort]);
+}
 </script> 
 @endsection
